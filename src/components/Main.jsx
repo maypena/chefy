1efea838a307391bfb377600bf830888da64a08b
May Pena <maypena00@gmail.com>
2025-09-07 12:09:00 -0400

    add submit handler

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,32 @@
 export default function Main() {
+    const ingridients = ["milk", "eggs", "flour"];
+
+    const ingListItems = ingridients.map(
+        (ingridient) => <li key={ingridient}>{ingridient}</li>
+    )
+    
+    function handleSubmit(e) {
+        e.preventDefault()
+        const formData = new FormData(e.target);
+        const ingridient = formData.get("ingridient");
+        ingridients.push(ingridient);
+        console.log(ingridients);
+    }
+
     return (
         <main>
-            <form className="add_ingridient_form">
+            <form onSubmit={handleSubmit} className="add_ingridient_form">
                 <input 
                     type="text"
                     placeholder="e.g. sugar"
                     aria-label="Add ingridient"
+                    name="ingridient"
                 />
                 <button>+ Add ingridient</button>
             </form>
+            <ul>
+                {ingListItems}
+            </ul>
         </main>
     )
 }
